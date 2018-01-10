6b03c2390a321a3366d64f1d9dd596ca85fa0666
vorosanda <33096441+vorosanda@users.noreply.github.com>
2018-01-10 18:15:08 +0200

    cam

---
+++ cam.js
@@ -0,0 +1,21 @@
+ar d = new Date();
+
+document.getElementById("id_business_level_version").innerHTML = 
+		"Business level version:" + 
+		d.getFullYear() + "." + d.getMonth() + 1 + "." + d.getDate() + ".0"; 
+
+var constraints = {audio:true, video:true};		
+navigator.mediaDevices.getUserMedia(constraints).then(on_success).catch(on_error);
+
+var video = document.getElementById("id_video");
+//---------------------------------
+function on_success(stream)
+{
+	video.srcObject = stream;
+}
+//---------------------------------
+function on_error(error)
+{
+	alert("Error");
+}
+//---------------------------------
